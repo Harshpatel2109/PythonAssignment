2d022ac64531522151270c7db49afe7fe27c248e
Harshpatel2109 <hp210903@gmail.com>
2026-02-09 23:44:44 +0530

    Assignment 2

---
+++ Assignment_2_Task_1.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print(f"{num} is an even number.")
+# else:
+#     print(f"{num} is an odd number.")
+print(f'{num} is an even number.') if num % 2 == 0 else print(f'{num} is an odd number.')
---
+++ Assignment_2_Task_2.py
@@ -0,0 +1,6 @@
+start = 1
+end = 50
+total = 0
+for i in range(start, end + 1):
+    total += i
+print(f"The sum of numbers from {start} to {end} is {total}")
